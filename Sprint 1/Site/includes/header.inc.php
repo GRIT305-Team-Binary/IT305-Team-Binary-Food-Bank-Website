436d044f9b35f70328526dc83f3b9f703f840b89
Jami <jamischwarzwalder@gmail.com>
2016-02-03 01:35:20 -0800

    Added a footer fixed some Validation errors

--- Sprint 1/Site/includes/header.inc.php
+++ Sprint 1/Site/includes/header.inc.php
@@ -7,59 +7,74 @@
     <meta name="author" content="">
 
     <title>Kent Food Bank & Emergency Services</title>
-    
-    
-    
+
+
+
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="readable_css/bootstrap.css" rel="stylesheet">
+    <link href="readable_css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    
+
     <!-- Custom CSS -->
     <link href="css/styles.css" rel="stylesheet">
 
-    
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
- 
-    <div class="container">
+
+    <div class="container-fluid head">
       <!---Top of the page-->
 
+    <div class="row">
+        <div id="logo" class="hidden-sm col-md-3 col-xs-12">
+            <a href="index.php" class="navbar-brand" ><img alt="This is the logo" src="images/logo-transparent.png" ></a>
+        </div>
 
-      <div id="logo" class=" col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <a href="#" class="navbar-brand"><img alt="This is the logo" src="../images/logo.png"></a>
-      </div>
-
-      <nav class="col-lg-8 col-md-8 col-sm-9 col-sx-12 navbar center-block">
-          <div class="container"><!--Centers Nav On Headers-->
+      <div class="col-xs-12 col-sm-12 col-md-8 navbar center-block">
+          <div class="container-fluid"><!--Centers Nav On Headers-->
 
             <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse" >Menu<span class="caret"></span></button>
             <div class="collapse navbar-collapse navHeaderCollapse">
               <ul class="nav navbar-nav">
-                  <li class="nav"><a href="#"><i class="fa fa-home fa-3x"></i><span id="navEl">Home</span></a></li>
-                  <li class="nav"><a href="#"><i class="fa fa-users fa-3x"></i><span id="navEl">Contribute</span></a></li>
-                  <li class="nav"><a href="#"><i class="fa fa-cutlery fa-3x"></i><span id="navEl">Get Help</span></a></li>
-                  <li class="nav"><a href="#"><i class="fa fa-calendar fa-3x"></i><span id="navEl">Calendar</span></a></li>
-                  <li class="nav"><a href="#"><i class="fa fa-map-marker fa-3x"></i><span id="navEl">Location</span></a></li>
-                  <li class="nav"><a href="#"><i class="fa fa-phone-square fa-3x"></i><span id="navEl">Contact Us</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="index.php"><i class="text-center icon-in-nav fa fa-home fa-3x"></i><span class="text-center nav-fonts">Home</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="contribute.php"><i class="text-center icon-in-nav fa fa-users fa-3x"></i><span class="text-center nav-fonts">Contribute</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="gethelp.php"><i class="text-center icon-in-nav fa fa-cutlery fa-3x"></i><span class="text-center nav-fonts text-nowrap">Get Help</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="calendar.php"><i class="text-center icon-in-nav fa fa-calendar fa-3x"></i><span class="text-center nav-fonts">Calendar</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="location.php"><i class="text-center icon-in-nav fa fa-map-marker fa-3x"></i><span class="text-center nav-fonts">Location</span></a></li>
+                  <li class="nav  col-sm-2 col-md-2 col-lg-2 col-xl-2"><a href="contactus.php"><i class="text-center icon-in-nav fa fa-phone-square fa-3x"></i><span class="text-center nav-fonts text-nowrap">Contact Us</span></a></li>
               </ul>
             </div>
-        </div>
-      </nav>
+          </div>
+    </div>
 
-      <a href="#" id="donateBtn" class="btn btn-md btn-default col-lg-1 col-md-1 hidden-sm"><span class="glyphicon glyphicon-credit-card"></span> Donate</a>
+
+    <div class="hidden-sm col-md-1 hidden-xs">
+        <a  class="hidden-sm" href="#" id="donateBtn" class="btn btn-md btn-default ">
+        <!--<span class="glyphicon glyphicon-credit-card"></span> Donate -->
+        <!--Paypal Donate Button-->
+        <div style="width: auto">
+            <form action="https://www.paypal.com/cgi-bin/webscr (Links to an external site.)" method="post" target="_top">
+
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="HPNP9YXHUXN4G">
+            <input type="image"   class="img-responsive" src="images/paypaldonate.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+             <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+
+            </form>
+        </div>
+    </div>
+
+      </a>
       <div><hr class="col-md-12 col-sm-12 hidden-xs"></div>
 
-      <div id="moto" class="col-md-5 col-md-offset-7 col-sm-7 col-sm-offset-5"><p>Serving low-income residents of the Kent School District</p></div>
-
-
+      <div id="motto" class="pull-right"><p>Serving low-income residents of the Kent School District</p></div>
+</div>
+</div>
       <!--Bottom of the page-->
     </div>
-      <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
-
-      </script>
-
