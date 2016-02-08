db3ed2933aa63518d026b89efc1c5888f8c1b0ba
Jami <jamischwarzwalder@gmail.com>
2016-02-07 21:55:28 -0800

    Changed styles to blue and orange

---
+++ Sprint 2/Site/gethelp.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php  include ('includes/header.inc.php');  ?>
+
+
+	 <div class="main" id="top">
+    <div class="container-fluid">
+        <h1>Get Help</h1>
+		
+		
+     <!-- Side navigation -->
+ <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 " role="group">
+            <div class="btn-group-vertical col-xs-12">
+				 <!-- Contact Us Button on mobile site -->
+                <a href="contactus.php" class="btn btn-warning visible-xs">Contact Us</a>
+				 <!-- Services We Provide header displays on larger screens -->
+                <a href="#services" class="btn btn-warning ">Our Services</a>
+				
+                <a href="#clothingbank" class="btn btn-default">Clothing Bank</a>
+                <a href="#foodbankcard" class="btn btn-default">Food Bank Card</a>
+				<div class="btn btn-warning">Other Support</div>
+				<!--Community Services that might be helpful to clients of Food Bank -->
+				<a href="https://www.uwkc.org/need-help/" class="btn btn-default" target="_blank">United Way</a>
+				<a href="https://www.kcls.org/esl/" class="btn btn-default" target="_blank">ESL</a>
+				<a href="http://crisisclinic.org/education/2-1-1-community-resources/" class="btn btn-default" target="_blank">2-1-1</a>
+				<a href="https://www.dshs.wa.gov/esa/community-services-offices/community-services-office" class="btn btn-default" target="_blank">D S H S</a><br>
+            
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-9" >
+            <!-- Main Content -->
+            <div class="col-lg-7 ">
+				<img src="images/building.JPG" id="buildingImg">
+            <!-- List of main services available from Kent Food Bank -->
+            <h3 id="services">Services We Provide</h3>
+            <ul>
+                <li>Food and clothing to low-income residents of the Kent School District, comprised of ZIP codes 98030 and 98031, and parts of 98032, 98042 and 98058.</li>
+                <li>Each household may have two visits to the Food Bank per month</li>
+                <li>Remember to bring your own bags or boxes to carry your food</li>
+            </ul>
+            <!-- Link to top of Page 
+			<div class="row"><div class="col-xs-6 col-xs-offset-6 visible-xs">
+			<p><span class="top-link-block right">
+				<a href="#top" class="well well-sm" >
+					<i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+				</a>
+				</span>
+				</div>
+			</div>
+			<!-- /top-link-block -->
+            <!-- Information about the Clothing Bank-->
+            
+            <h3 id="clothingbank">Clothing Bank</h3>
+			<ul>
+            <li>10 item limit per persion in family </li>
+            <li>Diapers available once every 6 months if available</li>
+			</ul><br>
+			<p><img src="images/clothingbank.png" id="buildingImg" class="img-repsonsive"></p>
+            </div>
+			
+			
+            <div class="col-lg-5 pull-left" >
+            <!-- Items client needs to bring into food bank to obtain a food card -->
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                  <h3 class="panel-title text-center" id="foodbankcard">How to Obtain a Food Bank Card</h3>
+                </div>
+                <div class="panel-body">
+                
+            <!--<h3 id="foodbankcard">How to Obtain a Food Bank Card</h3> -->
+            <h4>Bring:</h4>
+            <ul>
+                <li>Picture identification</li>
+                <li>Current proof of address  (if available)</li>
+            </ul>
+            <p>to <a href="https://goo.gl/maps/r75mDcuyd2J2">Kent Food Bank</a> during <a href="calendar.php">open hours</a>
+            to obtain your Food Bank Card.</p><br />
+            <h5>If registering a household, you need the following for each member of the household: </h5>
+            
+            <h4>For each adult: </h4>
+            <p>Current mail, lease, or bill not older than 30 days</p>
+ 
+            <h4>For each school-aged child:  </h4>
+            <p>Current piece of mail or proof of enrollment in Kent School District</p>
+ 
+            <h4>For younger children:  </h4>
+            <p>Birth Certificate</p>
+              Panel content
+                </div>
+              </div>
+			<!-- Link to top of Page 
+			<div class="row"><div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-8 visbile-sm visible-xs visible-md">
+			<p><span class="top-link-block right">
+				<a href="#top" class="well well-sm" >
+					<i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+				</a>
+				</span>
+				</div>
+			</div><!-- /top-link-block -->
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    
+ <?php  include ('includes/footer.php');  ?>
---
+++ Sprint 2/Site/location.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+        <?php
+            include('includes/header.inc.php');
+        ?>
+        <div class="main">
+            <h1>Locations</h1>
+            <div class="row">
+                 <div class="col-xs-12 col-sm-3">
+                 <!-- Orange Side Panel Style -->
+                 <div class="panel panel-warning">
+                <div class="panel-heading">
+                  <h3 class="panel-title text-center" id="foodbankcard">Come Visit Us</h3>
+                </div>
+                <div class="panel-body"> 
+                    <!-- Option #2 for Header 
+                    <br><p id="hoursOperation">Location</p>-->
+                    
+                    <h3>Main Food Bank</h3>
+                    <p>515 W Harrison St, Ste 107 <br>
+                    Kent, Washington 98032</p>
+                    
+                    <h3>Clothing Bank</h3>
+                    <p>515 W Harrison St, Ste 107 <br>
+                    Kent, Washington 98032</p>
+                    <h3>Birch Creek Annex*</h3>
+                    <p>12961 SE 275th Street<br>
+                    <span class="asterisk">* Food Bank services only</span></p><br>
+                </div>
+                </div> 
+                 </div>
+            
+                <div class="col-sm-9 hidden-xs pull-right" >
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.5137724658625!2d-122.24086688437721!3d47.382407679170406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905bfdbff8f409%3A0x4dd23f30dde84be0!2sKent+Food+Bank+%26+Emergency!5e0!3m2!1sen!2sus!4v1454306759479"
+                             height="550" style="border:0"  width="100%" allowfullscreen></iframe>
+                </div>
+                
+                <div class="col-xs-12 visible-xs">
+                    <p>Get directions with <a href="https://goo.gl/maps/r75mDcuyd2J2">Google Maps</a>.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                <h3>Parking</h3>
+                <p> Please park in legal designated parking spots to avoid tickets and towing!</p>
+                <p> HomeStreet Bank and Key Bank are private lots. Parking in their lots could result in your vehicle being towed at your own expense. </p>
+                </div>
+            </div>
+        </div>
+        <?php
+            include('includes/footer.php');
+        ?>
