b2152c7b86c0e1476bf118e6a9fc679bbd3c3652
Jami <jamischwarzwalder@gmail.com>
2016-02-12 17:53:58 -0800

    attempt to add styles to Contact Us Form

--- Sprint 2/Site/contactus.php
+++ Sprint 2/Site/contactus.php
@@ -1,3 +1,29 @@
+<?php
+$errors = [];
+$missing = [];
+if (isset($_POST['send'])) {
+    $expected = ['name', 'email', 'subject', 'comments'];
+    $required = ['name', 'email', 'comments'];
+    $recipient = 'Nicole Bassen <nicolerbassen@gmail.com>';
+	if (!empty($_POST['subject'])) {
+		$subject = $_POST['subject'];
+	}
+    else {
+		$subject = 'Feedback from online form';
+	}
+    $headers = [];
+    $headers[] = 'From: webmaster@example.com';
+    $headers[] = 'Cc: another@example.com';
+    $headers[] = 'Content-type: text/plain; charset=utf-8';
+    $authorized = null;
+    require './includes/process_mail.php';
+    if ($mailSent) {
+        header('Location: contact-thank-you.php');
+        exit;
+    }
+}
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -7,18 +33,14 @@
 	<div class="main">
     <div class="container-fluid ">
         <div class="row">
-            <div class="col-xs-12 ">
+            <div class="col-xs-12 col-lg-offest-1 col-lg-10">
                 <h2>Contact Us</h2>
             </div>
         </div>
-		<div class="row">
-			
-		</div>
+	
         <div class="row ">
-			
 			<div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-				 
-				  <!-- Contact Form -->
+			  <!-- Contact Form 
 				 
 				 <form method="POST" action="contact-form-process.php">
 					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -42,11 +64,112 @@
 						</div>
 					</div>
 				 </form>
+		-->
+			<?php if ($_POST && ($suspect || isset($errors['mailfail']))) : ?>
+			<p>Sorry, your mail couldn't be sent.</p><br>
+			<?php elseif ($errors || $missing) : ?>
+			<p>Please fix the item(s) indicated:</p>
+			<?php endif; ?>
+			
+			<form method="post" action="<?= $_SERVER['PHP_SELF']; ?>">
+			 <div class="row ">
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-left">
+					<div class="form-group">
+						<p>
+							<label for="name">Name*
+								<?php if ($missing && in_array('name', $missing)) : ?>
+								<span class="formError">Please enter your name.</span>
+								<?php endif; ?>
+							</label><br>
+							<div class="col-xs-12">
+							<input type="text" name="name" id="name"  class="input form-control "
+							<?php
+								if ($errors || $missing) {
+									echo 'value="' . htmlentities($name) . '"';
+								}
+							?>
+							>
+							</div>
+						</p>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right form-group">
+					
+					<div class="form-group">
+						<p><label for="email">E-mail address*
+							<?php if ($missing && in_array('email', $missing)) : ?>
+							<span class="formError">Please enter your email address.</span>
+							<?php elseif (isset($errors['email'])) : ?>
+							<span class="formError ">Invalid email address</span>
+							<?php endif; ?>
+						</label><br>
+						<div class="col-xs-12">
+							<input type="email" name="email" id="email" 	 class="input form-control"					<?php
+								if ($errors || $missing) {
+									echo 'value="' . htmlentities($email) . '"';
+								}
+							?> >
+						</div>
+						</p>
+					</div>
+				</div>
+			</div>
+			  
+				<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+						<div class="form-group">
+						<p><label for="subject">Subject</label><br>
+						<input type="subject" name="subject" id="subject" class="form-control"
+						<?php
+							if ($errors || $missing) {
+								echo 'value="' . htmlentities($subject) . '"';
+							}
+						?>
+						</p>
+						</div>
+					</div>
+				
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+						<div class="form-group">
+		
+							<p>
+								<label for="comments">Comments*
+									<?php if ($missing && in_array('comments', $missing)) : ?>
+									<span class="formError">You forgot to add any comments.</span>
+									<?php endif; ?>
+								</label><br>
+								<textarea name="comments" id="comments" rows="5" class="input form-control"><?php
+									if ($errors || $missing) {
+										echo htmlentities($comments);
+									}
+								?></textarea>
+								
+							</p>
+						</div>
+					</div>
+					<p>
+						* = Required field<br><br>
+						<input type="submit" name="send" id="send" value="Send Comments">
+					</p>
+				</div>
+			
+				</div>
+			
+			</form>
+				 
+				<?php
+					if ($_POST && $mailSent) {
+						echo "<div class='row '>
+								<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center'>
+										Your message has been sent. Thank you!
+								</div>";
+					}
+				?>
 				 
 				 
-			 </div>
-			
-			
+			</div>
+				
 			
 			        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 " role="group">
 						<div class="btn-group-vertical col-xs-12">
@@ -89,7 +212,7 @@
 		
 		 </div>
 	    </div>
-    </div>
+  
 	
  <?php
             include('includes/footer.php');
