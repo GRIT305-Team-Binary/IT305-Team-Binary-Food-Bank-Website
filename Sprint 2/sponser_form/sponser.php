e543c26939510aa32652b2cea6a213cb78d37ff4
Peter Kim <PeterLKim1337@gmail.com>
2016-02-15 01:27:09 -0800

    Newer and improved sponser_form also should have removed css clonflict

--- Sprint 2/sponser_form/sponser.php
+++ Sprint 2/sponser_form/sponser.php
@@ -16,126 +16,125 @@
   margin-top: 20px;
 }
 
-button {
-  margin: 20px 0;
+ #sponserFormSubmitBtn {
+  width: 30%;
+  height: 60px;
   border-radius: 5px;
-  box-shadow: 5px 5px 2.5px #888888;
+  box-shadow: 2px 2px 2.5px #888888;
+  margin-top: 20px;
 }
 
-h2, h3{
+
+.sponserBoxHeader{
   text-align: center;
 }
 
-h3{
+h3.sponserBoxHeader{
   text-decoration: underline;
   margin-bottom: 20px;
 }
 
+.sponserBox{
+    border-radius: 2.5px;
+    margin-top: 15px;
+    border: 1px solid black;
+    box-shadow: 2px 2px 2.5px #888888;
+}
+
+.sponserFormTextBoxes{
+  border-radius: 2.5px;
+  padding: 15px .5% 15px .5%;
+  margin-top: 20px;
+  border: 1px solid black;
+  box-shadow: 2px 2px 2.5px #888888;
+}
+
+.sponserLiEl li{
+  list-style-type: none;
+}
+
+.sponser-radio-btn{
+  width: 30px;
+  height: 30px;
+}
+
+.rdoTxt{
+  position: relative;
+  bottom: 10px;
+}
+
+.sponserRadioBtnNTxt{
+  width: 63%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 /* Custom, iPhone Retina */
     @media only screen and (min-width : 320px) and (max-width : 479px) {
-      .sponserFormTextBoxes{
-        border-radius: 5px;
-        padding: 15px .5% 15px .5%;
-        margin-top: 20px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
-      }
-
-      #sponserFormSubmitBtn{
-        margin-top: 20px;
-        height: 60px;
-      }
 
       .sponserFormTextBoxes{
         position: relative;
         top: 10px;
-        width: 180px;
         float: none;
         display: table;
         margin: 0 auto;
 
       }
-      .sponserBox{
-        border-radius: 5px;
-        margin-top: 15px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
-      }
+
+    .inputFields{
+      text-align: center;
+    }
+
+    #sponserFormSubmitBtn {
+    position: relative;
+    left: 19%;
+   }
+
     }
 
     /* Extra Small Devices, Phones */
     @media only screen and (min-width : 480px) and (max-width : 767px) {
 
-      .sponserFormTextBoxes{
-        border-radius: 5px;
-        padding: 15px .5% 15px .5%;
-        margin-top: 20px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
+      .inputFields{
+        text-align: center;
       }
 
-      #sponserFormSubmitBtn{
-        margin-top: 20px;
-        height: 60px;
-      }
+      #sponserFormSubmitBtn {
+      position: relative;
+      left: 18%;
+     }
 
-      .sponserBox{
-        border-radius: 5px;
-        margin-top: 15px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
-      }
 
     }
 
     /* Small Devices, Tablets */
     @media only screen and (min-width : 768px) and (max-width : 991px) {
 
-      .sponserFormTextBoxes{
-        border-radius: 5px;
-        padding: 15px .5% 15px .5%;
-        margin-top: 20px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
-      }
-
-      #sponserFormSubmitBtn{
-        margin-top: 20px;
-        height: 60px;
-      }
-
       .sponserBox{
-        border-radius: 5px;
-        margin-top: 15px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
+        margin-left: 2% !important;
+        width: 31% !important;
+        height: 700px;
       }
 
+      .rdoTxt{
+        position: relative;
+        bottom: 25px;
+        left: 25%;
+        font-size: 1em;
+      }
     }
 
     /* Medium Devices, Desktops */
     @media only screen and (min-width : 992px) {
-      .sponserFormTextBoxes{
-        border-radius: 5px;
-        padding: 15px .5% 15px .5%;
-        margin-top: 20px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
-      }
 
-      #sponserFormSubmitBtn{
-        margin-top: 20px;
-        height: 60px;
+      .sponserRadioBtnNTxt{
+        width: 78%;
       }
 
       .sponserBox{
-        border-radius: 5px;
-        margin-top: 15px;
-        border: 1px solid black;
-        box-shadow: 5px 5px 2.5px #888888;
         margin-left: 2% !important;
         width: 31% !important;
-        height: 515px;
+        height: 520px;
       }
 
     }
@@ -148,17 +147,17 @@ h3{
   <p class="col-md-10 col-md-offset-1 sponserHeaderText">We are excited this year to provide you with a number of different options to support the Kent Food Bank Annual Breakfast with your monetary donations. We have two different payment options, a one-time sponsorship payment or monthly installments. Both options will make a direct difference for families in need.
   </p>
 
-  <p class=" col-md-12 sponserHeaderText"><b>Your generosity is greatly appreciated!
+  <p class="col-md-12 sponserHeaderText"><b>Your generosity is greatly appreciated!
   THANK YOU!!!
   </b></p>
 </div>
 
 <form class="form-horizontal">
 
-<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-3 col-md-offset-1 sponserBox">
-      <h2>Gold</h2>
-      <h3>$3,000 or $250 per month for a year</h3>
-        <ul>
+<div class="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-1 sponserBox">
+      <h2 class="sponserBoxHeader">Gold</h2>
+      <h3 class="sponserBoxHeader">$3,000 or $250 per month for a year</h3>
+        <ul class="sponserLiEl">
             <li>Logo and name recognition on printed materials</li><br>
             <li>Logo and name recognition in annual report</li><br>
             <li>Logo and name recognition on our Facebook page</li><br>
@@ -166,94 +165,93 @@ h3{
             <li>Each Table of Honor guest will receive 7 raffle tickets</li><br>
             <li>Certificate of Appreciation</li><br>
         </ul>
-        <input type="radio" name="sponserLvl" value="gold"><label>&nbsp;Donte at a Gold Level.</label>
+
+        <div class="sponserRadioBtnNTxt">
+          <input class="sponser-radio-btn" type="radio" name="sponserLvl" value="gold"><label class="rdoTxt">&nbsp;Donte at a Gold Level.</label>
+        </div>
 </div>
 
-<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-3 col-md-offset-1 sponserBox">
-      <h2>Silver</h2>
-      <h3>$1,500 or $125 per month for a year</h3>
-        <ul>
+<div class="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-1 sponserBox">
+      <h2 class="sponserBoxHeader">Silver</h2>
+      <h3 class="sponserBoxHeader">$1,500 or $125 per month for a year</h3>
+        <ul class="sponserLiEl">
             <li>Logo and name recognition on printed materials</li><br>
             <li>Name recognition in annual report</li><br>
             <li>Logo and name recognition on our Facebook page</li><br>
             <li>Verbal recognition day of event</li><br>
             <li>Each Table of Honor guest will receive 3 raffle tickets</li><br>
         </ul>
-        <input type="radio" name="sponserLvl" value="silver"><label>&nbsp;Donte at a Silver Level.</label>
+        <br class="hidden-xs hidden-md hidden-lg"/>
+        <br class="hidden-xs hidden-lg " />
+        <br class="hidden-xs"/>
+        <br class="hidden-xs "/>
+
+        <div class="sponserRadioBtnNTxt">
+          <input class="sponser-radio-btn" type="radio" name="sponserLvl" value="silver"><label class="rdoTxt">&nbsp;Donte at a Silver Level.</label>
+        </div>
 </div>
 
-<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-3 col-md-offset-1 sponserBox">
-      <h2>Bronze</h2>
-      <h3>$1,000 or $85 per month for a year</h3>
-        <ul>
+<div class="col-xs-10 col-xs-offset-1 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-1 sponserBox">
+      <h2 class="sponserBoxHeader">Bronze</h2>
+      <h3 class="sponserBoxHeader">$1,000 or $85 per month for a year</h3>
+        <ul class="sponserLiEl">
             <li>Name recognition on printed materials</li><br>
             <li>Name recognition in annual report</li><br>
             <li>Name recognition on our Facebook page</li><br>
             <li>Verbal recognition day of event</li><br>
             <li>Table of Honor at event</li><br>
         </ul>
-        <input type="radio" name="sponserLvl" value="bronze"><label>&nbsp;Donte at a Bronze Level.</label>
+        <br class="hidden-xs hidden-md hidden-lg"/>
+        <br class="hidden-xs hidden-md hidden-lg"/>
+        <br class="hidden-xs hidden-md hidden-lg"/>
+        <br class="hidden-xs hidden-md hidden-lg"/>
+        <br class="hidden-xs" />
+        <br class="hidden-xs" />
+        <br class="hidden-xs" />
+        <br  class="hidden-xs" />
+        <br class="hidden-md hidden-xs" />
+        <div class="sponserRadioBtnNTxt">
+          <input class="sponser-radio-btn" type="radio" name="sponserLvl" value="bronze"><label class="rdoTxt">&nbsp;Donte at a Bronze Level.</label>
+      </div>
   </div>
 
-<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 sponserFormTextBoxes" id="">
+<div class="col-xs-10 col-xs-offset-1  col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 sponserFormTextBoxes" id="">
   <div class="row">
-   <div class="col-xs-6  col-sm-4 col-md-4">
-      <label>First Name:</label><br>
-      <input type="text" name="fName">
+    <div class="inputFields col-xs-12  col-sm-4 col-md-4">
+      &nbsp;<label>First Name:</label><br />
+      &nbsp;<input type="text" name="fName">
    </div>
 
-    <div class="col-xs-6  col-sm-4 col-md-4">
-      <label>Last Name:</label><br>
-      <input type="text" name="lName">
+    <div class="inputFields col-xs-12  col-sm-4 col-md-4">
+      &nbsp;<label>Last Name:</label><br>
+      &nbsp;<input type="text" name="lName">
     </div>
 
-    <div class="col-xs-6  col-sm-4 col-md-4">
-      <label>Middle Initial:</label><br />
-      <input type="text" name="lName">
+    <div class="inputFields col-xs-12  col-sm-4 col-md-4">
+      &nbsp;<label>Middle Initial:</label><br />
+      &nbsp;<input type="text" name="lName">
     </div>
   </div>
 
   <div class="row">
-    <div class="col-xs-6 col-sm-4 col-md-4">
-      <label>Business/Organization:</label><br />
-      <input type="text" name="bus-org">
+    <div class="inputFields col-xs-12 col-sm-4 col-md-4">
+      &nbsp;<label>Business/Organization:</label><br />
+      &nbsp;<input type="text" name="bus-org">
     </div>
 
-    <div class="col-xs-6 col-sm-4 col-md-4">
-      <label>Phone:</label><br />
-      <input type="text" name="phone">
+    <div class="inputFields col-xs-12 col-sm-4 col-md-4">
+      &nbsp;<label>Phone:</label><br />
+      &nbsp;<input type="text" name="phone">
     </div>
 
-    <div class="col-xs-6  col-sm-4 col-md-4">
-      <label>Email:</label><br />
-      <input type="text" name="email">
+    <div class="inputFields col-xs-12  col-sm-4 col-md-4">
+      &nbsp;<label>Email:</label><br />
+      &nbsp;<input type="text" name="email">
     </div>
   </div>
 </div>
 
-<button class="col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4" id="sponserFormSubmitBtn">Submit Form</button>
+  <input type="submit" name="submit" class="col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4" id="sponserFormSubmitBtn">
+
 </form>
 </div>
-
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>
-      $(document).ready(function(){
-          $(window).load(function(){
-
-            var windowSize = $(window).width();
-
-            if(windowSize < 480){
-              $(".col-xs-6").css("width", "100%");
-            }
-          });
-          $(window).resize(function(){
-            var windowSize = $(window).width();
-
-            if(windowSize < 480){
-              $(".col-xs-6").css("width", "100%");
-            }
-
-          });
-      });
-
-</script>
