5bb9cd7b491ba339a26a7f69e89fdb041a741eb9
Peter Kim <PeterLKim1337@gmail.com>
2016-02-17 11:29:32 -0800

    merged (not git hub merge) nicoles footer with my footer

--- Sprint 2/Site/includes/footer.php
+++ Sprint 2/Site/includes/footer.php
@@ -1,61 +1,60 @@
  <!-- Footer -->
-
  <div class="footer">
-           
+
         <footer>
-                      
+
               <div class="row text-center ">
               		<!-- Location -->
                      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
                     <div class="btn-lg btn-warning col-xs-12">
-				
-                            <p><a class="defaultLink foot-btn" href="location.php"  >
+
+                            <p class="footerBtn"><a class="defaultLink foot-btn" href="location.php"  >
                                     <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
                                     Location
                             </a></p>
 					</div>
-					
+
                 </div>
 					 <div class="visible-xs">
 					 <p>&nbsp;</p>
-				
+
 					</div>
                  <!-- Phone Number -->
-                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
+				 <div class="col-sm-4 col-md-4 col-lg-4 hidden-xs">
+					 <h3 class="footerTitle">Kent Food Bank</h3>
+					 <p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550<br>
+                    515 W Harrison St, Ste 107 <br />
+                    Kent, Washington 98032</p>
+                </div>
+                <div class="col-xs-12 visible-xs">
                     <div class="btn-lg btn-warning col-xs-12">
 							<!-- Phone -->
-							<p><a class="defaultLink foot-btn" href="tel:1-253-520-3550"  ><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550</a></p>
+							<p class="footerBtn"><a class="defaultLink foot-btn" href="tel:1-253-520-3550"  ><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550</a></p>
 					</div>
                 </div>
+
 				<div class="visible-xs">
 					 <p>&nbsp;</p>
-					
+
 					</div>
                  <!-- Contact -->
                 <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
                     <div class="btn-lg btn-warning col-xs-12">
 							<!-- Phone -->
-							<p><a class="defaultLink foot-btn" href="contactus.php" >
+							<p class="footerBtn"><a class="defaultLink foot-btn" href="contactus.php" >
                                    <span  class="glyphicon glyphicon-phone" aria-hidden="true"></span> Contact Us
                             </a></p>
 					</div>
                 </div>
             </div>
-            <div class="row text-center foot">
-               
-                      <!-- Address -->
-                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <p><strong>Kent Food Bank<br />
-                    515 W Harrison St, Ste 107 <br />
-                    Kent, Washington 98032</strong></p>
-                </div>
-           
-                <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 text-right asterisk">
+            <div class="row">
+                <div class="col-xs-12 text-right asterisk">
                     <p>Copyright &copy; 2016 Kent Food Bank. All Rights Reserved.</p>
                 </div>
             </div>
         </footer>
  </div>
+
 	<!--Bottom of the page-->
 
 	<!-- jQuery -->
@@ -90,7 +89,6 @@
          }});
 
     </script>
-
 
 
 
