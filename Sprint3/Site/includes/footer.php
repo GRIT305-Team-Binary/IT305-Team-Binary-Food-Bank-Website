281f5060124284152e7bbe2c649a644a280ed8f2
nicolebassen <nicolerbassen@gmail.com>
2016-02-24 18:27:10 -0800

    Footer links, add map to footer, footer design and format

--- Sprint3/Site/includes/footer.php
+++ Sprint3/Site/includes/footer.php
@@ -1,52 +1,51 @@
  <!-- Footer -->
  <div class="footer">
-
+           
         <footer>
-
+                      
               <div class="row text-center ">
-              		<!-- Location -->
-                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
-                    <div class="btn-lg btn-warning col-xs-12">
+					 <div class="col-xs-12 col-sm-6">
+							<!-- Address and phone number -->
+							<div class="col-xs-12">
+							<br>
+							<h4 class="footerTitle">Kent Food Bank & Emergency Services</h4>
+							<!--<p class="hidden-xs"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550<br>-->
+							<div class="col-sm-5 hidden-xs text-left">
+								 <p class="footerP text-left">Contribute<br>
+								 <div class="underline text-left"></div>
 
-                            <p class="footerBtn"><a class="defaultLink foot-btn" href="location.php"  >
-                                    <span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
-                                    Location
-                            </a></p>
-					</div>
+								   <a href="#" class="footerLink">Donate</a><br>
+								   <a href="volunteer.php" class="footerLink">Volunteer</a><br>
+								   <a href="sponsor.php" class="footerLink">Sponsorship</a>
+								 
+								 </p>
+							</div>
+							<div class="col-xs-12 col-sm-7 text-left">
+								   <p class="footerP text-left">Contact<br>
+								 <div class="underline"></div>
 
-                </div>
-					 <div class="visible-xs">
-					 <p>&nbsp;</p>
+								   <span class="fa fa-phone footerIcon" aria-hidden="true"></span><a href="tel:1-253-520-3550" class="footerLink"> (253) 520-3550 </a><br>
+								   <span class="fa fa-map-marker footerIcon" aria-hidden="true"></span><a href="location.php" class="footerLink"> 515 W Harrison St, Ste 107 Kent,
+										  Washington 98032</a><br>
+								   <span class="fa fa-envelope footerIcon" aria-hidden="true"></span><a href="contact.php" class="footerLink"> Contact Us</a><br>
+								   <span class="fa fa-clock-o footerIcon" aria-hidden="true"></span><a href="calendar.php" class="footerLink"> Hours</a><br>
 
-					</div>
-                 <!-- Phone Number -->
-				 <div class="col-sm-4 col-md-4 col-lg-4 hidden-xs">
-					 <h3 class="footerTitle">Kent Food Bank</h3>
-					 <p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550<br>
-                    515 W Harrison St, Ste 107 <br />
-                    Kent, Washington 98032</p>
-                </div>
-                <div class="col-xs-12 visible-xs">
-                    <div class="btn-lg btn-warning col-xs-12">
-							<!-- Phone -->
-							<p class="footerBtn"><a class="defaultLink foot-btn" href="tel:1-253-520-3550"  ><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> (253) 520-3550</a></p>
-					</div>
-                </div>
+							<br>
+							</p>  
+							</div>
+							
+							</div>
+							
+					 </div>
+			  
+					 <div class="col-xs-12 col-sm-6">
+							<!--<p class="text-left footerP"><small>515 W Harrison St, Ste 107 Kent, Washington 98032</small></p>-->
+					 <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.5137724658625!2d-122.24086688437721!3d47.382407679170406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54905bfdbff8f409%3A0x4dd23f30dde84be0!2sKent+Food+Bank+%26+Emergency!5e0!3m2!1sen!2sus!4v1454306759479"
+                             height="250" style="border:0"  width="100%" allowfullscreen></iframe>
+					 </div>
+					
+			  </div>
 
-				<div class="visible-xs">
-					 <p>&nbsp;</p>
-
-					</div>
-                 <!-- Contact -->
-                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 ">
-                    <div class="btn-lg btn-warning col-xs-12">
-							<!-- Phone -->
-							<p class="footerBtn"><a class="defaultLink foot-btn" href="contactus.php" >
-                                   <span  class="glyphicon glyphicon-phone" aria-hidden="true"></span> Contact Us
-                            </a></p>
-					</div>
-                </div>
-            </div>
             <div class="row">
                 <div class="col-xs-12 text-right asterisk">
                     <p>Copyright &copy; 2016 Kent Food Bank. All Rights Reserved.</p>
@@ -54,41 +53,16 @@
             </div>
         </footer>
  </div>
-
+ 
 	<!--Bottom of the page-->
-
+	
 	<!-- jQuery -->
     <script async src="js/jquery.js"></script>
 	<script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script async src="js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-
-    $(document).ready(function(){
-      var windowWidth = $(document).width();
-      var imageSize = windowWidth/4;
-
-      if(windowWidth > 767){
-        $("#logo").css("width", imageSize + "px");
-    }else{
-        $("#logo").css("width", "100%");
-    }
-
-    });
-
-
-        $(window).resize(function(){
-            var windowWidth = $(document).width();
-            var imageSize = windowWidth/4;
-
-        if(windowWidth > 767){
-            $("#logo").css("width", imageSize + "px");
-        }else{
-            $("#logo").css("width", "100%");
-         }});
-
-    </script>
+	
 
 
 
