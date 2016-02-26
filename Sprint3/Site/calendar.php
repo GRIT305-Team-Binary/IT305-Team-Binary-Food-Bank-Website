f710cc32632685bd0482fd3b9aba95b36a3912a3
Jami <jamischwarzwalder@gmail.com>
2016-02-25 22:52:04 -0800

    Added embed Responsive to the iframe

--- Sprint3/Site/calendar.php
+++ Sprint3/Site/calendar.php
@@ -5,57 +5,84 @@
         <?php
             include('includes/header.inc.php');
         ?>
+     <div class="container-fluid">
         <div class="main">
             <h1>Calendar & Hours</h1>
             <div class="row">
-                 <div class="col-xs-12 col-sm-3">
-                 <!-- Orange Side Panel Style
+                  <div class="col-xs-12 col-md-3">
+                 <!-- Orange Side Panel Style-->
+				 <ul class="nav nav-tabs">
+				  <li id="hours" class="active"><a href="">Hours</a></li>
+				  <li id="events"><a href="">Events</a></li>
+				</ul>
                  <div class="panel panel-warning">
                 <div class="panel-heading">
-                  <h3 class="panel-title text-center" id="foodbankcard">Hours of Operation</h3>
+					<div id="hours-title"><h3 class="panel-title text-center" >Hours of Operation</h3></div>
+					<div id="events-title"><h3 class="panel-title text-center" >Annual Events</h3></div>
                 </div>
-                <div class="panel-body"> -->
-                    <!-- Option #2 for Header -->
-                    <br><h4 class="title">Hours of Operation</h4>
+                <div class="panel-body"> 
+                   
+                    <div id="hours-body">
+                    <!-- List of Hours -->
+                    <?php  include ('includes/hours.php');  ?>
+					</div>
+					<div id="event-body">
+                    <!-- List of Annual Events -->
                     
-                    <h3>Food Bank</h3>
-                    <p><span class="dayOfWeek">Tuesday, Wednesday, Friday</span><br>
-                    10am - 1pm</p>
-                    <p><span class="dayOfWeek">Second Wednesday of each month</span><br>
-                    5pm - 6pm<br><br></p>
-                    <h3>Clothing Bank</h3>
-                    <p><span class="dayOfWeek">Tuesday, Wednesday, Friday</span><br>
-                    10am - 12:30pm<br><br></p>
-                    <h3>Birch Creek Annex*</h3>
-                    <p><span class="dayOfWeek">Monday</span><br>
-                    10am - 1pm<br>
-                    <span class="asterisk">* Food Bank services only</span></p><br>
-                <!-- </div>
-                </div> -->
+					</div>
+                   
+                 </div>
+                </div> 
                  </div>
             
                 <!-- calendar on med/large screen sizes -->
-                <div class="col-med-9 hidden-xs hidden-sm">
-                    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showTz=0&amp;wkst=1&amp;bgcolor=%23FF9600&amp;src=teambinarykfb%40gmail.com&amp;color=%23000000&amp;ctz=America%2FLos_Angeles"
-                            style="border-width:0" width="73%" height="600px" frameborder="0" scrolling="no"></iframe>
-                </div>
+                <div class="col-xs-12 col-md-9 ">
+					<div class="embed-responsive embed-responsive-16by9">
+
+
+                    <iframe class="embed-responsive-item"  src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showTz=0&amp;wkst=1&amp;bgcolor=%23FF9600&amp;src=teambinarykfb%40gmail.com&amp;color=%23000000&amp;ctz=America%2FLos_Angeles"
+                           scrolling="no"></iframe>
+					</div>
+				</div>
                 
-                <!-- calendar on small screen size -->
-                <div class="col-sm-9 visible-sm">
-                    <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FF9600&amp;src=teambinarykfb%40gmail.com&amp;color=%23000000&amp;ctz=America%2FLos_Angeles"
-                            style="border-width:0" width="100%" height="600px" frameborder="0" scrolling="no"></iframe>
-                </div>
                 
-                <!-- calendar on mobile/xs screen size -->
-                <div class="col-xs-12 visible-xs">
-                    <iframe src="https://calendar.google.com/calendar/embed?mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFF9600&amp;src=teambinarykfb%40gmail.com&amp;color=%23000000&amp;ctz=America%2FLos_Angeles"
-                             width="600"  scrolling="yes"></iframe>
-                </div>
                 
             </div>
         </div>
+     </div>
        <!-- Calendar and Hours Page  -->
    
         <?php
             include('includes/footer.php');
         ?>
+		<script src="http://code.jquery.com/jquery.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script>
+	  
+			$("div#events-title").hide();
+			$("div#events-body").hide();
+			$("div#hours-title").show();
+			$("div#hours-body").show();
+			
+			
+            $("li#events").click(function(){
+				
+            	$("div#hours-title").hide();
+				$("div#hours-body").hide();
+				$("div#events-title").show();
+				$("div#event-body").show();
+                $("li#hours").removeClass("active");
+				 $("li#events").addClass("active");
+				
+			});
+			
+			$("li#hours").click(function(){
+				
+			    $("div#events-title").hide();
+				$("div#event-body").hide();
+				$("div#hours-title").show();
+				$("div#hours-body").show();
+                $("li#events").removeClass("active");
+				$("li#hours").addClass("active");				
+			});
+        </script>
