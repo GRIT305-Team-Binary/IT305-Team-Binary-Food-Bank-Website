a463ea347329e3a7c6e6bbd47c8b9ceaa442b81b
Jami <jamischwarzwalder@gmail.com>
2016-02-25 22:59:39 -0800

    Removing .. from bottom of page

--- Sprint3/Site/contribute.php
+++ Sprint3/Site/contribute.php
@@ -18,15 +18,15 @@
     <div class="row">
         <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 " role="group">
              <?php  include ('includes/contribute-side.php');  ?>
-            
+
         </div>
- 
+
 
         <div class="topItemsList col-xs-12 col-sm-3 col-md-3 col-lg-3 pull-right">
-			
+
 			 <?php  include ('includes/top-items-needed.php');  ?>
            <p class="text-center"><a href="includes/top-items-needed.php"><span class="glyphicon glyphicon-print"></span>  Print</a></p>
-			<!-- Link to top of Page 
+			<!-- Link to top of Page
 			<div class="row"><div class="col-xs-6 col-xs-offset-6 visible-xs">
 			<p><span class="top-link-block right">
 				<a href="#top" class="well well-sm" >
@@ -36,15 +36,15 @@
 				</div>
 			</div> /top-link-block -->
         </div>
-       
-      
+
+
          <div class="col-xs-12 col-sm-5 col-md-5 col-lg-6 pull-left">
             <!-- Details about what Kent Food Bank does -->
-			
+
             <p>Each year Kent Food Bank distributes approximately 6,000 lbs of food.  We are a non-profit organization that runs on volunteer support with funding from grants, individual donors, and some money from the city of Kent.</p>
             <p>Every contribution makes a difference. </p>
             <p class="text-center hidden-xs"><a href="https://www.paypal.com/us/webapps/mpp/search-cause?charityId=75871&amp;s=3" class="btn btn-warning">Donate Today</a></p>
-			
+
             <!-- How you can help the Clothing Bank -->
             <h2> Non-Perishable Food</h2>
             <p>Kent Food Bank accepts nonperishable food donations on M, T, W and F from 9 am – 2 pm at</p>
@@ -56,7 +56,7 @@
         	<!-- Button for Mobile display-->
 			<p class="text-center  visible-xs visible-sm"><a href="location.php" class="btn btn-warning text-center">Kent Food Bank</a></p>
             <!-- Other ways to give to include Amazon Smile and Fred Meyer Rewards-->
-			<!-- Link to top of Page 
+			<!-- Link to top of Page
 			<div class="row"><div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-8 visible-xs visible-sm visible-md">
 			<p><span class="top-link-block right">
 				<a href="#top" class="well well-sm" >
@@ -70,20 +70,20 @@
             <h2 id="otherGift">Other ways to give:</h2>
             <h3>Amazon Smile</h3>
 			<p><a href="http://smile.amazon.com"><img src="images/AmazonSmile-logo.png" alt="Amazon Smile" class="img-responsive"></a></p>
-           
+
             <p>Amazon donates 0.5% of the price of your eligible AmazonSmile purchases to the charitable organization of your choice. </p>
-			<p>AmazonSmile is the same Amazon you know. Same products, same prices, same service.</p>	
+			<p>AmazonSmile is the same Amazon you know. Same products, same prices, same service.</p>
 			<p>Support your charitable organization by starting your shopping at</p>
 			<p class="text-center"><a href="http://smile.amazon.com" class="btn btn-warning">smile.amazon.com</a></p>
             <hr>
             <h3>Fred Meyer Community Rewards</h3>
 			<p><a href="http://www.fredmeyer.com/communityrewards"><img src="images/fred-meyer-logo.png" alt="Fred Meyer Rewards" class="img-responsive"></a></p>
             <P>Sign up for the Community Rewards program by linking your Fred Meyer Rewards Card to Kent Food Bank at:
-			<p class="text-center hidden-xs "><a href="http://www.fredmeyer.com/communityrewards" class="btn btn-warning">FredMeyer.com/communityrewards</a></p>.
+			<p class="text-center hidden-xs "><a href="http://www.fredmeyer.com/communityrewards" class="btn btn-warning">FredMeyer.com/communityrewards</a></p>
 			<!-- Button for Mobile display-->
-			<p class="text-center visible-xs"><a href="http://www.fredmeyer.com/communityrewards" class="btn btn-warning">Fred Meyer Rewards</a></p>.
+			<p class="text-center visible-xs"><a href="http://www.fredmeyer.com/communityrewards" class="btn btn-warning">Fred Meyer Rewards</a></p>
 			<p class="text-center bold"><strong>You can search for us by name, Kent Food Bank or by our NPO # 83698</strong></p>
-			<!-- Link to top of Page 
+			<!-- Link to top of Page
 			<div class="row"><div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-8">
 			<p><span class="top-link-block right">
 				<a href="#top" class="well well-sm" >
@@ -92,11 +92,10 @@
 			</span>
 			</div>
 			</div> /top-link-block -->
-			
+
 				</div>
 			</div>
 		</div>
 	 </div>
 
-<?php  include ('includes/footer.php');  ?>
-
+ <?php  include ('includes/footer.php');  ?>
