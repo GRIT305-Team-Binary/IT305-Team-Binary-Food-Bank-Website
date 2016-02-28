81002035d684736c6fa098f08d96d6070a80e591
Jami <jamischwarzwalder@gmail.com>
2016-02-27 23:03:33 -0800

    Adding attribution at the top of pages and fixing indentation

--- Sprint3/Site/contactus.php
+++ Sprint3/Site/contactus.php
@@ -26,161 +26,151 @@ if (isset($_POST['send'])) {
 <!DOCTYPE html>
 
 <html lang="en">
-	<?php
-		include ('includes/header.inc.php');
-	?>
-	<div class="main">
-    <div class="container-fluid ">
-        <div class="row">
-            <div class="col-xs-12 col-lg-offest-1 col-lg-10">
-                <h2>Contact Us</h2>
-            </div>
-        </div>
-	
-        <div class="row ">
+<?php
+include ('includes/header.inc.php');
+?>
+<div class="main">
+	<div class="container-fluid ">
+		<div class="row">
+			<div class="col-xs-12 col-lg-offest-1 col-lg-10">
+				<h2>Contact Us</h2>
+			</div>
+		</div>
+		<div class="row ">
 			<div class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
-
-			<?php if ($_POST && ($suspect || isset($errors['mailfail']))) : ?>
-				<h2 class="formError">Sorry, your mail couldn't be sent.</h2><br>
-			<?php elseif ($errors || $missing) : ?>
-				<h2 class="formError">Please fix the item(s) indicated:</h2>
-			<?php endif; ?>
-			<?php
+				<?php if ($_POST && ($suspect || isset($errors['mailfail']))) : ?>
+					<h2 class="formError">Sorry, your mail couldn't be sent.</h2><br>
+				<?php elseif ($errors || $missing) : ?>
+					<h2 class="formError">Please fix the item(s) indicated:</h2>
+				<?php endif; ?>
+				<?php
 					if ($_POST && $mailSent) {
-						echo "<div class='row '>
-								<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center'>
-										<p class='form-confirm'>Your message has been sent. Thank you!</p>
-								</div>
-							</div>";
+					echo "<div class='row '>
+					<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center'>
+					<p class='form-confirm'>Your message has been sent. Thank you!</p>
+					</div>
+					</div>";
 					}
 				?>
-			
-			<form method="post" action="<?= $_SERVER['PHP_SELF']; ?>" role="form">
-			 <div class="row ">
-				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-left">
-					<div class="form-group">
-						
+				<form method="post" action="<?= $_SERVER['PHP_SELF']; ?>" role="form">
+					<div class="row ">
+					
+						<!-- Name -->
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-left">
+							<div class="form-group">
 								<div class="col-xs-12">
 									<p>
-								<label for="name">
-								<?php if ($missing && in_array('name', $missing)) : ?>
-								<span class="formError">Please enter your name.</span>
-								<?php endif; ?>
-								<br>Name*
-							</label><br>
+										<label for="name">
+											<?php if ($missing && in_array('name', $missing)) : ?>
+											<span class="formError">Please enter your name.</span>
+											<?php endif; ?>
+											<br>Name*
+										</label><br>
+										<!-- adding class if error -->
+										<input type="text" name="name" id="name"  class="input form-control 
+										<?php
+											if ($errors || $missing) {
+												echo 'has-error has-feedback" ';
+												// adding value of previoulsy entered
+												echo 'value="' . htmlentities($name) . '"';
+											} else {
+												echo '"';
+											}
+										?>
+										>
+									</p>
+								</div> <!-- End of Name div-->
+							</div> <!-- End of Form group -->
+						</div><!-- End of div col-md-6 -->
 						
-							<input type="text" name="name" id="name"  class="input form-control 
-							<?php
-								if ($errors || $missing) {
-									echo 'has-error has-feedback" ';
-									echo 'value="' . htmlentities($name) . '"';
-								} else {
-									echo '"';
-								}
-							?>
-							>
-							</p>
-							</div>
-						
-					</div>
-				</div>
-				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right form-group">
+						<!-- Email -->
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 pull-right form-group">
+							<div class="form-group">
+								<div class="col-xs-12">
+									<p>
+										<label for="email" class="control-label">
+											<?php if ($missing && in_array('email', $missing)) : ?>
+											<span class="formError">Please enter your e-mail.</span>
+											<?php elseif (isset($errors['email'])) : ?>
+											<span class="formError ">Invalid email address</span>
+											<?php endif; ?>
+											<br>E-mail address*
+										</label><br>
 					
-					<div class="form-group">
-						
-							<div class="col-xs-12">
-							<p>
-								<label for="email" class="control-label">
-							<?php if ($missing && in_array('email', $missing)) : ?>
-							<span class="formError">Please enter your e-mail.</span>
-							<?php elseif (isset($errors['email'])) : ?>
-							<span class="formError ">Invalid email address</span>
-							<?php endif; ?>
-							<br>E-mail address*
-						</label><br>
-						
-							<input type="email" name="email" id="email" class="input form-control"
-							<?php
-								if ($errors || $missing) {
-									echo 'value="' . htmlentities($email) . '">';
-									
-								} else {
-									echo 'value="' . htmlentities($email). '">'; 
-									
-								}	
-							?> 
-						</p>
-					    </div>
-						
-					</div>
-				</div>
-			</div>
-			  
-				<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-						<div class="form-group">
-						<p><label for="subject">Subject</label><br>
-						<input type="text" name="subject" id="subject" class="form-control
-						<?php
-							if ($errors || $missing) {
-								echo 'has-error has-feedback" ';
-								echo 'value="' . htmlentities($subject) . '"';
-							} else {
-									echo '"';
-								}
-						?> >
-						</p>
-						</div>
-					</div>
+										<input type="email" name="email" id="email" class="input form-control"
+										<?php
+											if ($errors || $missing) {
+												echo 'value="' . htmlentities($email) . '">';
+											} else {
+												echo 'value="' . htmlentities($email). '">'; 
+											}	
+										?> 
+									</p>
+								</div> <!-- End of Email -->
+							</div> <!-- End of Form group -->
+						</div><!-- End of div col-md-6 -->
+					</div><!-- End of row-->
+					
+					<!-- Subject -->
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+								<div class="form-group">
+									<p>
+										<label for="subject">Subject</label><br>
+										<input type="text" name="subject" id="subject" class="form-control
+										<?php
+										if ($errors || $missing) {
+										echo 'has-error has-feedback" ';
+										echo 'value="' . htmlentities($subject) . '"';
+										} else {
+										echo '"';
+										}
+										?> >
+									</p>
+								</div><!-- End of form group -->
+							</div> <!-- End of div col-lg-12 -->
+						</div> <!-- End of div col-lg-12 -->
+					</div> <!-- End of row -->
 				
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
-						<div class="form-group">
-		
-							<p>
-								<label for="comments">Comments*
-									<?php if ($missing && in_array('comments', $missing)) : ?>
-									<span class="formError">You forgot to add any comments.</span>
-									<?php endif; ?>
-								</label><br>
-								<textarea name="comments" id="comments" rows="5" class="input form-control"><?php
-								//If php is not on the line above it ands strange white space
-									if ($errors || $missing) {
-										
-										echo htmlentities($comments);
-									}
-								?></textarea>
-								
-							</p>
-						</div>
-					</div>
+					<!-- Comments -->
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+							<div class="form-group">
+								<p>
+									<label for="comments">Comments*
+										<?php if ($missing && in_array('comments', $missing)) : ?>
+											<span class="formError">You forgot to add any comments.</span>
+										<?php endif; ?>
+									</label><br>
+									<textarea name="comments" id="comments" rows="5" class="input form-control">
+										<?php
+											//If php is not on the line above it ands strange white space
+											if ($errors || $missing) {
+												echo htmlentities($comments);
+											}
+										?>
+									</textarea>
+								</p>
+							</div><!-- End of form group-->
+						</div> <!-- End of div col-lg-12 -->
+					</div> <!-- End of row -->	
 					<p>
 						* = Required field<br><br>
 						<input type="submit" name="send" id="send" value="Send Comments">
 					</p>
-				</div>
+				</form><!-- End of Form -->
+			</div><!-- End of div col-lg-7 -->
 			
-				</div>
-			
-			</form>
-				 
-				
-				 
-				 
+			<!-- Side bar with Contact information for Kent Food Bank -->
+			<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 " role="group">
+				<?php
+				include('includes/contact_KFB.php');
+				?>	
 			</div>
-				
-			
-			        <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 " role="group">
-					 <?php
-						include('includes/contact_KFB.php');
-					?>	
-			 </div>
- 
-		
-		 </div>
-	    </div>
-  
-		</div>
- <?php
-            include('includes/footer.php');
-        ?>
+		</div><!-- End of row -->
+	</div><!-- End of container-fluid -->
+</div><!-- End of main -->
+<?php
+include('includes/footer.php');
+?>
--- Sprint3/Site/gethelp.php
+++ Sprint3/Site/gethelp.php
@@ -11,135 +11,132 @@
 
 <?php  include ('includes/header.inc.php');  ?>
 
-
 <div class="container-fluid" >
 	<div class="main" id="top" >
-	<h1>Get Help</h1>
-	
-	
-	<!-- Side navigation -->
-	<div class="row">
-	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 " role="group">
-	<div class="btn-group-vertical col-xs-12">
-	<!-- Contact Us Button on mobile site -->
-	<a href="contactus.php" class="btn btn-warning visible-xs">Contact Us</a>
-	<!-- Services We Provide header displays on larger screens -->
-	<a href="#services" class="btn btn-warning ">Our Services</a>
-	
-	<a href="#clothingbank" class="btn btn-default">Clothing Bank</a>
-	<a href="#foodbankcard" class="btn btn-default">Food Bank Card</a>
-	<div class="btn btn-warning">Other Support</div>
-	<!--Community Services that might be helpful to clients of Food Bank -->
-	<a href="https://www.uwkc.org/need-help/" class="btn btn-default" target="_blank">United Way</a>
-	<a href="https://www.kcls.org/esl/" class="btn btn-default" target="_blank">ESL</a>
-	<a href="http://crisisclinic.org/education/2-1-1-community-resources/" class="btn btn-default" target="_blank">2-1-1</a>
-	<a href="https://www.dshs.wa.gov/esa/community-services-offices/community-services-office" class="btn btn-default" target="_blank">D S H S</a><br>
-	
-	</div>
-	<p>&nbsp;</p>
-	<!--<div class="col-xs-12">
-	<h3>Hours of Service</h3>	-->
-	<div class="panel panel-warning">
-	<div class="panel-heading">
-	<h3 class="panel-title text-center" id="hours">Hours of Operation</h3>
-	</div>
-	<div class="panel-body">
-	<!-- List of Hours -->
-	<?php  include ('includes/hours.php');  ?>
-	</div>
-	</div>
-	</div>
-	<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9" >
-	<!-- Main Content -->
-	<div class="col-lg-7 ">
-	<h3>Main Location</h3>
-	<a href="location.php"><img class="img-responsive buildingImg" src="images/building.JPG" alt="Main Location"></a>
-	<!-- List of main services available from Kent Food Bank -->
-	<h3 id="services">Services We Provide</h3>
-	<ul>
-	<li>Food and clothing to low-income residents of the Kent School District, comprised of ZIP codes 98030 and 98031, and parts of 98032, 98042 and 98058.</li>
-	<li>Each household may have two visits to the Food Bank per month</li>
-	<li>Remember to bring your own bags or boxes to carry your food</li>
-	</ul>
-	
-	
-	<!-- Link to top of Page -->
-	<!-- <div class="row"><div class="col-xs-6 col-xs-offset-6 visible-xs">
-	<p><span class="top-link-block right">
-	<a href="#top" class="well well-sm" >
-	  <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
-	</a>
-	</span>
-	</div>
-	</div>
-	/top-link-block-->
-	
-	
-	
-	
-	<!-- Information about the Clothing Bank-->
-	
-	<h3 id="clothingbank">Clothing Bank</h3>
-	<ul>
-	<li>One visit per month </li>
-	<li>10 item limit per person in family </li>
-	<li>Diapers available once every 6 months if available</li>
-	</ul><br>
-	<p><img src="images/clothingbank.png" class="img-repsonsive buildingImg" alt="Clothing Bank"></p>
-	
-	<h3>Birch Creek Annex</h3>
-	<ul>
-	  <li>Only perishable and refrigerated food available </li>
-	  <li>Alternative to visiting Main Location</li>
-	</ul>
-	<p>For more information see <a href="location.php">Locations</a></p>
-	<a href="location.php"><img class="img-responsive buildingImg" src="images/annex.png" alt="Birch Creek Annex"></a>
-	<p>&nbsp;</p>
-	</div>
-	
-	
-	<div class="col-lg-5 pull-left" >
-	<!-- Items client needs to bring into food bank to obtain a food card -->
-	<div class="panel panel-warning">
-	<div class="panel-heading">
-	<h3 class="panel-title text-center" id="foodbankcard">How to Obtain a Food Bank Card</h3>
-	</div>
-	<div class="panel-body">
-	
-	<h3>Obtain Eligibility</h3> 
-	<h4>Bring:</h4>
-	<ul>
-	<li>Picture identification</li>
-	<li>Current proof of address  (if available)</li>
-	</ul>
-	<p>to <a href="https://goo.gl/maps/r75mDcuyd2J2">Kent Food Bank</a> during <a href="calendar.php">open hours</a>
-	to obtain your Food Bank Card.</p><br />
-	<h5>If registering a household, you need the following for each member of the household: </h5>
-	
-	<h4>For each adult: </h4>
-	<p>Current mail, lease, or bill not older than 30 days</p>
-	
-	<h4>For each school-aged child:  </h4>
-	<p>Current piece of mail or proof of enrollment in Kent School District</p>
-	
-	<h4>For younger children:  </h4>
-	<p>Birth Certificate</p>
-	
-	</div>
-	</div>
-	<!-- Link to top of Page 
-	<div class="row"><div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-8 visbile-sm visible-xs visible-md">
-	<p><span class="top-link-block right">
-	<a href="#top" class="well well-sm" >
-	  <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
-	</a>
-	</span>
-	</div>
-	</div> /top-link-block -->
-	</div>
-	</div>
-	</div>
-	</div>
+		<h1>Get Help</h1>
+		
+		
+		<!-- Side navigation -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 " role="group">
+				<div class="btn-group-vertical col-xs-12">
+					<!-- Contact Us Button on mobile site -->
+					<a href="contactus.php" class="btn btn-warning visible-xs">Contact Us</a>
+					<!-- Services We Provide header displays on larger screens -->
+					<a href="#services" class="btn btn-warning ">Our Services</a>
+					<a href="#clothingbank" class="btn btn-default">Clothing Bank</a>
+					<a href="#foodbankcard" class="btn btn-default">Food Bank Card</a>
+					<div class="btn btn-warning">Other Support</div>
+					
+					<!--Community Services that might be helpful to clients of Food Bank -->
+					<a href="https://www.uwkc.org/need-help/" class="btn btn-default" target="_blank">United Way</a>
+					<a href="https://www.kcls.org/esl/" class="btn btn-default" target="_blank">ESL</a>
+					<a href="http://crisisclinic.org/education/2-1-1-community-resources/" class="btn btn-default" target="_blank">2-1-1</a>
+					<a href="https://www.dshs.wa.gov/esa/community-services-offices/community-services-office" class="btn btn-default" target="_blank">D S H S</a><br>
+					
+				</div>
+				<p>&nbsp;</p>
+				<!--<div class="col-xs-12">
+				<h3>Hours of Service</h3>	-->
+				<div class="panel panel-warning">
+					<div class="panel-heading">
+						<h3 class="panel-title text-center" id="hours">Hours of Operation</h3>
+					</div>
+					<div class="panel-body">
+						<!-- List of Hours -->
+						<?php  include ('includes/hours.php');  ?>
+					</div>
+				</div>
+			</div> <!-- End side panel -->
+			<div class="col-xs-12 col-sm-8 col-md-8 col-lg-9" >
+				<!-- Main Content -->
+				<div class="col-lg-7 ">
+					<h3>Main Location</h3>
+					<a href="location.php"><img class="img-responsive buildingImg" src="images/building.JPG" alt="Main Location"></a>
+					<!-- List of main services available from Kent Food Bank -->
+					<h3 id="services">Services We Provide</h3>
+					<ul>
+						<li>Food and clothing to low-income residents of the Kent School District, comprised of ZIP codes 98030 and 98031, and parts of 98032, 98042 and 98058.</li>
+						<li>Each household may have two visits to the Food Bank per month</li>
+						<li>Remember to bring your own bags or boxes to carry your food</li>
+					</ul>
+					<!-- Link to top of Page -->
+					<!-- <div class="row"><div class="col-xs-6 col-xs-offset-6 visible-xs">
+					<p><span class="top-link-block right">
+					<a href="#top" class="well well-sm" >
+					  <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+					</a>
+					</span>
+					</div>
+					</div>
+					/top-link-block-->
+					
+					
+					
+					
+					<!-- Information about the Clothing Bank-->
+					
+					<h3 id="clothingbank">Clothing Bank</h3>
+					<ul>
+						<li>One visit per month </li>
+						<li>10 item limit per person in family </li>
+						<li>Diapers available once every 6 months if available</li>
+					</ul><br>
+					<p><img src="images/clothingbank.png" class="img-repsonsive buildingImg" alt="Clothing Bank"></p>
+					<!-- Information about the Birch Creek Annex-->
+					<h3>Birch Creek Annex</h3>
+					<ul>
+					  <li>Only perishable and refrigerated food available </li>
+					  <li>Alternative to visiting Main Location</li>
+					</ul>
+					<p>For more information see <a href="location.php">Locations</a></p>
+					<a href="location.php"><img class="img-responsive buildingImg" src="images/annex.png" alt="Birch Creek Annex"></a>
+					<p>&nbsp;</p>
+				</div><!-- End of main content-->
+				
+				
+				<!-- Items client needs to bring into food bank to obtain a food card -->
+				<div class="col-lg-5 pull-left" >
+					<div class="panel panel-warning">
+						<div class="panel-heading">
+							<h3 class="panel-title text-center" id="foodbankcard">How to Obtain a Food Bank Card</h3>
+						</div>
+						<div class="panel-body">
+						
+							<h3>Obtain Eligibility</h3> 
+							<h4>Bring:</h4>
+							<ul>
+							<li>Picture identification</li>
+							<li>Current proof of address  (if available)</li>
+							</ul>
+							<p>to <a href="https://goo.gl/maps/r75mDcuyd2J2">Kent Food Bank</a> during <a href="calendar.php">open hours</a>
+							to obtain your Food Bank Card.</p><br />
+							<h5>If registering a household, you need the following for each member of the household: </h5>
+							
+							<h4>For each adult: </h4>
+							<p>Current mail, lease, or bill not older than 30 days</p>
+							
+							<h4>For each school-aged child:  </h4>
+							<p>Current piece of mail or proof of enrollment in Kent School District</p>
+							
+							<h4>For younger children:  </h4>
+							<p>Birth Certificate</p>
+						
+						</div><!-- End of panel Body -->
+					</div><!-- End of panel  -->
+					<!-- Link to top of Page 
+					<div class="row"><div class="col-xs-6 col-xs-offset-6 col-sm-4 col-sm-offset-8 visbile-sm visible-xs visible-md">
+					<p><span class="top-link-block right">
+					<a href="#top" class="well well-sm" >
+					  <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
+					</a>
+					</span>
+					</div>
+					</div> /top-link-block -->
+				</div> <!-- End of right panel -->
+			</div>
+		</div><!-- end of row -->
+	</div><!-- end of main -->
 </div> <!-- End of container-fluid -->
 
 <?php  include ('includes/footer.php');  ?>
