dba3791293dd3cbf1f843feb52cfba8edaeebfe5
Jami <jamischwarzwalder@gmail.com>
2016-03-15 01:43:33 -0700

    Nicole added question about availability

--- Sprint 4/Site/volunteer.php
+++ Sprint 4/Site/volunteer.php
@@ -11,7 +11,8 @@
 	//All of the fields expected to have values from form
 	$form_fields = ['appType', 'fname', 'lname', 'address', 'city', 'zip', 'phone', 'email',
 					 'clothing', 'office', 'food', 'drive', 'whyVolunteer',
-					  'lift', 'questions', 'crime'];
+					  'lift', 'questions', 'crime', 'monday', 'tuesday', 'wednesday', 'thursday',
+					  'friday', 'weekends'];
 	//'crime' & 'lift' is added below if they have court ordered community service
 	
 	//Create placeholder text for entry forms
@@ -33,7 +34,7 @@
 		//All of the fields expected to have values from form
 		$expected = ['appType', 'fname', 'lname', 'address', 'city', 'zip', 'phone', 'email',
 					 'clothing', 'office', 'food', 'drive', 'whyVolunteer',
-					  'questions'];
+					  'questions', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'weekends'];
 				
 		//All of the fields required to have user entered content in the form
 		$required = ['appType','fname', 'lname', 'address', 'city', 'zip', 'phone', 'email',
@@ -48,8 +49,8 @@
 		$isValid = true;
 		
 		 //Validate Checkboxes for Volunteer Opportunities
-		if ($_POST && empty($_POST['clothing']) && empty($_POST['office']) && empty($_POST['food'])) {
-			array_push ( $required , 'clothing', 'office', 'food' );
+		if ($_POST && empty($_POST['clothing']) && empty($_POST['office']) && empty($_POST['food']) && empty($_POST['drive'])) {
+			array_push ( $required , 'clothing', 'office', 'food', 'drive' );
 			$isValid = false;
 		} else {
 			$isValid = true;
@@ -575,6 +576,70 @@ include ('includes/header.inc.php');
 						</fieldset>
 
 					</div>
+			   </div>
+			   
+			   <div class="row">
+				<div class="col-xs-12" >
+				<fieldset class="form-group">
+
+						   <label>Which days are you available? (check all that apply):</label>
+						   <div class="checkbox">
+							   <label for="monday">
+							   <input type="checkbox" name="monday" id="monday"
+							   <?php if ($monday) : ?>
+								checked
+								<?php endif; ?>
+							   > M&nbsp;&nbsp;&nbsp;
+							   <!-- if Monday was selected, add checked to the input field -->
+							   </label>
+
+							   <label for="tuesday">
+							   <!-- if Tuesday was selected, add checked to the input field -->
+							   <input type="checkbox" name="tuesday" id="tuesday"
+							   <?php if ($tuesday) : ?>
+								checked
+								<?php endif; ?>
+							   > T&nbsp;&nbsp;&nbsp;
+							   </label>
+
+							   <label for="wednesday">
+							   <!-- if Wednesday was selected, add checked to the input field -->
+								<input type="checkbox" name="wednesday" id="wednesday"
+							   <?php if ($wednesday) : ?>
+								checked
+								<?php endif; ?>
+							  > W&nbsp;&nbsp;&nbsp;
+							   </label>
+							   
+							   <label for="thursday">
+							   <!-- if Thursday was selected, add checked to the input field -->
+							   <input type="checkbox" name="thursday" id="thursday"
+							   <?php if ($thursday) : ?>
+								checked
+								<?php endif; ?>
+							   > Th&nbsp;&nbsp;&nbsp;
+							   </label>
+							   
+							   <label for="friday">
+							   <!-- if Friday was selected, add checked to the input field -->
+							   <input type="checkbox" name="friday" id="friday"
+							   <?php if ($friday) : ?>
+								checked
+								<?php endif; ?>
+							   > F&nbsp;&nbsp;&nbsp;
+							   </label>
+							   
+							   <label for="weekends">
+							   <!-- if Weekends was selected, add checked to the input field -->
+							   <input type="checkbox" name="weekends" id="weekends"
+							   <?php if ($weekends) : ?>
+								checked
+								<?php endif; ?>
+							   > Weekends
+							   </label>	   
+						   </div>
+					   </fieldset>
+				</div>
 			   </div>
 			   <div class="row">
 					<!-- Text Area for Additional Comments/Questions -->
